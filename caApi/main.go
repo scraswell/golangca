9c036f231f5e472420661a1209aac052d86ad1b5
Sean Craswell <sean.craswell@customer.io>
2023-02-05 16:16:50 -0400

    route changes.

--- caApi/main.go
+++ caApi/main.go
@@ -1,27 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/scraswell/golangca/authority"
 	"net/http"
 )
 
-const ListRootCertificatesRoute = "/rootCerts"
-const ListIntermediateCertificatesRoute = "/intCerts"
+const ListRootCertificates = "/list/root"
+const ListIntermediateCertificates = "/list/int"
+const GetCertificate = "/get"
 
 func main() {
 	router := gin.Default()
-	router.GET(ListRootCertificatesRoute, func(ctx *gin.Context) {
+
+	router.GET(ListRootCertificates, func(ctx *gin.Context) {
 		ctx.JSON(
 			http.StatusOK,
 			authority.ListRootCertificates())
 	})
 
-	router.GET(ListIntermediateCertificatesRoute, func(ctx *gin.Context) {
+	router.GET(ListIntermediateCertificates, func(ctx *gin.Context) {
 		ctx.JSON(
 			http.StatusOK,
 			authority.ListIntermediateCertificates())
 	})
 
-	router.Run()
+	err := router.Run()
+	if err != nil {
+		panic(fmt.Errorf("failed to start router %w", err))
+	}
 }
